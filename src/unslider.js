8f76b8a2a9aebb716dab4cbb66a23a7bc92ed95d
hho2002 <hho2002@gmail.com>
2014-03-31 13:53:48 +0800

    add fading functionality

--- src/unslider.js
+++ src/unslider.js
@@ -28,7 +28,8 @@
 			items: '>ul',   // slides container selector
 			item: '>li',    // slidable items selector
 			easing: 'swing',// easing function to use for animation
-			autoplay: true  // enable autoplay on initialisation
+			autoplay: true, // enable autoplay on initialisation
+			fade: false     //use fade instead of slide
 		};
 
 		_.init = function(el, o) {
@@ -65,6 +66,12 @@
 			//ul.css({position: 'relative', left: 0, width: (len * 100) + '%'});
 			ul.css({position: 'relative', left: 0, width: (_.max[0] * len) + 'px'});
 			li.css({'float': 'left', width: (_.max[0]) + 'px', height: 'auto'});
+
+			if (o.fade) {
+				//li.css({'position': 'absolute', 'display': 'none'});
+				li.css({'display': 'none'});
+				ul.find("li").eq(0).css({'display': 'block'});
+			}
 
 			//  Autoslide
 			o.autoplay && setTimeout(function() {
@@ -158,11 +165,19 @@
 				//  Handle those pesky dots
 				el.find('.dot').eq(index).addClass('active').siblings().removeClass('active');
 
-				el.animate(obj, speed, easing) && ul.animate($.extend({left: '-' + index + '00%'}, obj), speed, easing, function(data) {
-					_.i = index;
+				if (o.fade) {
+					if (_.i != index) {
+						_.i = index;
+						ul.find("li:visible").fadeOut(speed / 2);
+						ul.find("li").eq(index).fadeIn(speed / 2);
+					}
+				} else {
+					el.animate(obj, speed, easing) && ul.animate($.extend({left: '-' + index + '00%'}, obj), speed, easing, function(data) {
+						_.i = index;
 
-					$.isFunction(o.complete) && !callback && o.complete(el, target);
-				});
+						$.isFunction(o.complete) && !callback && o.complete(el, target);
+					});
+				}
 			};
 		};
 
